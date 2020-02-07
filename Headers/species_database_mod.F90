a1f05b297623aee6966202bc77148311ae84988f
Maria Paula Pérez-Peña <mpperezpena@hotmail.com>
2020-02-07 11:25:50 +1100

    Update species_database_mod.F90

--- Headers/species_database_mod.F90
+++ Headers/species_database_mod.F90
@@ -1656,7 +1656,7 @@ CONTAINS
              Is_Wetdep     = F
              Is_Photolysis = T
 
-          CASE( 'HNO3' )
+          CASE( 'z' )
 
              !%%% NOTE: HNO3 dry-deposits like a gas, but wet-deposits
              !%%% like an aerosol.  Therefore we need to define HNO3
@@ -5745,7 +5745,9 @@ CONTAINS
 
           CASE( 'H2' )
              FullName      = 'Molecular hydrogen'
+             ! Modified by M.Perez-Pena in Jan. 2020
              Formula       = 'H2'
+             MW_g          = 2.0_fp
              BackgroundVV  = 5.0e-07_fp
              Is_Gas        = T
              Is_Drydep     = F
